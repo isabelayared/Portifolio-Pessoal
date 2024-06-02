e0a8d294d15edac8b6d30752bb2bd2335ba0979b
isabelayared <167128156+isabelayared@users.noreply.github.com>
2024-06-02 18:37:18 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,26 @@
+
+
+    const moonImg = document.getElementById('moon-img');
+    const body = document.body;
+
+    moonImg.addEventListener('click', () => {
+        // Toggle between moon and sun images
+        if (moonImg.src.includes('lua')) {
+            moonImg.src = '/html, css, assets/assets/sol.png'; // Path to your sun image
+        } else {
+            moonImg.src = '/html, css, assets/assets/lua.png'; // Path to your moon image
+        }
+
+        // Toggle between light and dark mode
+        body.classList.toggle('dark-mode');
+    });
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+
